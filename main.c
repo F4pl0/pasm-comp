b87ffe359763f5d28b8fbe32e548d84527eca48b
f4 <straka.filip@gmail.com>
2020-11-10 21:40:28 +0100

    Add basic argument parser

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
+enum {
+    FLAG_FILE = 1
+} Flag;
+
+char *FILE_NAME = "";
+bool v_output = false;
+
+int main(int argc, char *argv[]) {
+    // pasm -c file.c
+    enum Flag nextFLag = (enum Flag) NULL;
+
+    for (int i = 0; i < argc; ++i) {
+        if (nextFLag == FLAG_FILE) {
+            FILE_NAME = argv[i];
+            nextFLag = (enum Flag) NULL;
+        } else if (strcmp(argv[i], "-f") == 0) {
+            nextFLag = FLAG_FILE;
+        } else if (strcmp(argv[i], "-v") == 0) {
+            v_output = true;
+        }
+    }
+
     return 0;
 }
