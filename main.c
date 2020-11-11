42dff4ed7033682095119bf2241c682ca49b66b5
f4 <straka.filip@gmail.com>
2020-11-11 17:53:45 +0100

    Add main.c with basic flag processing

--- main.c
+++ main.c
@@ -1,28 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include "pasm.h"
+#include "utils.h"
 
 enum {
     FLAG_FILE = 1
 } Flag;
 
-char *FILE_NAME = "";
+char *SOURCE_FILE_NAME = "";
 bool v_output = false;
 
+/* Let's assume the user would use the flags which contain a certain value as
+ *  pasm -f file.c
+ */
 int main(int argc, char *argv[]) {
-    // pasm -c file.c
+
+    // Parse the input flags
     enum Flag nextFLag = (enum Flag) NULL;
 
     for (int i = 0; i < argc; ++i) {
         if (nextFLag == FLAG_FILE) {
-            FILE_NAME = argv[i];
+            SOURCE_FILE_NAME = argv[i];
             nextFLag = (enum Flag) NULL;
         } else if (strcmp(argv[i], "-f") == 0) {
             nextFLag = FLAG_FILE;
         } else if (strcmp(argv[i], "-v") == 0) {
             v_output = true;
+            //printv("Verbose mode enabled.\n");
         }
     }
 
+    // Load in the source file
+    FILE *source_file = NULL;
+    fopen_s(&source_file , SOURCE_FILE_NAME, "r"); // read only
+
+    // Check if the file is open
+    if ( !source_file ) {
+        printf("Unable to open %s\n", SOURCE_FILE_NAME);
+        return 1;
+    } else if ( source_file != NULL && v_output ) {
+        printf("Successfully opened %s\n", SOURCE_FILE_NAME);
+    }
+
+    // Read the file and store it in temp. buffer
+    int CUR_MAX = 4095;
+    char *buffer = (char*) malloc(sizeof(char) * CUR_MAX); // allocate buffer.
+    int length = 1;
+
+    int ch = getc(source_file);
+    buffer[0] = (char)ch;
+
+    while ( ch != EOF ) {
+        if(length == CUR_MAX) { // time to expand ?
+            CUR_MAX *= 2; // expand to double the current size of anything similar.
+            buffer = realloc(buffer, CUR_MAX); // re allocate memory.
+        }
+        ch = getc(source_file); // read from stream.
+        buffer[length] = (char)ch; // stuff in buffer.
+        length++;
+    }
+
+    pasm_compile(buffer, length);
+
+    free(buffer);
     return 0;
 }
